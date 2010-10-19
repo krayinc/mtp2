5be71723dd856e50f463f73d09c7742ddc030e6c
func09 <mitsuru.haga@gmail.com>
2010-10-19 12:57:16 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtp2::Application.config.secret_token = '467b43a48109b00c1639a90249392ce384aff8ed67fb05498b8274374e2e4ef1582586a45daaec1f8a638999abc53485b13abad4864a5720ee9c40dd1b0ea52a'
