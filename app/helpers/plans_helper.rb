822a5fc444c058bb1362c9041440cfefcbb5d8eb
Takatoshi MORIYAMA <hawk@at-exit.com>
2010-11-12 22:33:33 +0900

    * トップページ・プラン一覧の写真をランダムで表示するように修正

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -2,4 +2,15 @@ module PlansHelper
   def plan_status_label(status)
     Plan::Status.rassoc(status).to_a[0]
   end
+
+  def plan_photo_list(plan, opts = {})
+    opts = {
+      :max => 6,
+    }.merge(opts)
+    result = ''
+    plan.photos.shuffle[0...opts[:max]].each do |photo|
+      result << photo_tag(photo.photo, :list, :link_to_original => true)
+    end
+    raw result
+  end
 end
